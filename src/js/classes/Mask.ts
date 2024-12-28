fa8b5a957b73c339a61c98f83838d2a4f939ebfe
GoldenEy3-maker <danil-danil-korolev@bk.ru>
2024-12-28 15:57:49 -0800

    feat: add phone mask

--- src/js/classes/Mask.ts
+++ src/js/classes/Mask.ts
@@ -1,21 +1,22 @@
 import Inputmask from "inputmask";
 
 export default class Mask {
-    selector: string
-    constructor(selector: string) {
-        this.selector = selector
-    }
+  selector: string;
+  constructor(selector: string) {
+    this.selector = selector;
+    this.init();
+  }
 
-    init() {
-        new Mask('.js-mask-phone').phone({})
-    }
+  init() {
+    this.phone({});
+  }
 
-    phone(options: any) {
-        return new Inputmask({
-            ...options,
-            mask: '+7 (999) 999 99-99',
-            showMaskOnHover: false,
-            autoUnmask: true
-        }).mask(this.selector)
-    }
+  phone(options: any) {
+    return new Inputmask({
+      ...options,
+      mask: "+7 (999) 999 99-99",
+      showMaskOnHover: false,
+      autoUnmask: true,
+    }).mask(this.selector);
+  }
 }
